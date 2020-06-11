9594fe1b47106c2c852f7cf62d2a83650a1e044e
wiarda <wiarda@gmail.com>
2020-06-11 22:14:30 +0800

    Refactor countMisses

--- src/Puzzle.hs
+++ src/Puzzle.hs
@@ -5,6 +5,8 @@ import Data.Maybe (isJust)
 import Control.Monad (forever)
 import System.Exit (exitSuccess)
 
+maxGuesses = 7
+
 data Puzzle = Puzzle String [Maybe Char] [Char]
 
 instance Show Puzzle where
@@ -44,24 +46,26 @@ parseGuess p g = do
       putStrLn "You've already guessed this letter!"
       return result
     (True, _) -> do
-      putStrLn "You've matched a letter!"
+      putStrLn "You matched a letter!"
       return result
     (False, _) -> do
-      putStrLn "Bad guess!"
+      putStrLn "Uh oh, try again!"
       return result
+
+
+countMisses :: Puzzle -> Int
+countMisses (Puzzle word _ guesses) = 
+  length . filter (\x -> not $ elem x word) $ guesses
 
 
 gameOver :: Puzzle -> IO ()
-gameOver (Puzzle word _ guessed) = 
-  if (missedGuesses > 7) then
+gameOver p@(Puzzle word _ guessed) = 
+  if (countMisses p > 7) then
     do 
-      putStrLn "You guessed too many times! Game over!"
+      putStrLn "You guessed incorrectly too many times :("
       putStrLn $ "The word was:" ++ word
       exitSuccess
   else return ()
-  where 
-    missedGuesses =
-      (length . filter (\x -> not $ elem x word)) guessed
 
 gameWon :: Puzzle -> IO ()
 gameWon (Puzzle _ filledInSoFar _) =
